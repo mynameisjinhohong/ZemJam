9aed1d808604dc716947d41781b154aba55834da
amm-10 <wlsdn3740@naver.com>
2026-06-21 03:37:12 +0900

    타임라인 이벤트 연결?

--- Assets/Script/CutScene/SimpleCutSceneStarter.cs
+++ Assets/Script/CutScene/SimpleCutSceneStarter.cs
@@ -5,9 +5,16 @@ public class SimpleCutSceneStarter : MonoBehaviour
 {
     [SerializeField] private string _cutSceneKey;
 
+    [SerializeField] private GameObject _selectImage;
+
     private void Start()
     {
         CutSceneManager.Instance.ShowUISequence("GuideE1", "GuideE2", "GuideE3");
+    }
+
+    public void ShowE0Bubble()
+    {
+        CutSceneManager.Instance.ShowUIOnly("GuideE0", ShowSelectImage);
     }
 
     public void StartCutScene()
@@ -18,5 +25,10 @@ public class SimpleCutSceneStarter : MonoBehaviour
         {
             CutSceneManager.Instance.Play(_cutSceneKey);
         }
+    }
+
+    private void ShowSelectImage()
+    {
+        _selectImage.SetActive(true);   
     }
 }
