4120316513718cff1b434992644f7c9fc02733fa
amm-10 <wlsdn3740@naver.com>
2026-06-21 03:13:49 +0900

    엔딩 대사 수정 마지막 클릭씬 대사

--- Assets/Script/CutScene/SimpleCutSceneStarter.cs
+++ Assets/Script/CutScene/SimpleCutSceneStarter.cs
@@ -5,8 +5,15 @@ public class SimpleCutSceneStarter : MonoBehaviour
 {
     [SerializeField] private string _cutSceneKey;
 
+    private void Start()
+    {
+        CutSceneManager.Instance.ShowUISequence("GuideE1", "GuideE2", "GuideE3");
+    }
+
     public void StartCutScene()
     {
+        if(GameManager.Instance.CanPlayerMove == false) return;
+
         if(CutSceneManager.Instance != null)
         {
             CutSceneManager.Instance.Play(_cutSceneKey);
